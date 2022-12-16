e2dde65419b762c1edd339e81ca12983548799fb
Andre Fredette <afredette@redhat.com>
2022-12-16 14:07:27 -0500

    This code is an example of how to save and reconstruct a tc link
    It is based on aya with the following prs:
    https://github.com/aya-rs/aya/pull/462
    https://github.com/aya-rs/aya/pull/445
    (w/445 modified to work with 462)
    
    Signed-off-by: Andre Fredette <afredette@redhat.com>

--- tchandle/src/main.rs
+++ tchandle/src/main.rs
@@ -1,18 +1,25 @@
+use anyhow::anyhow;
+use anyhow::Result;
 use aya::programs::tc::{SchedClassifierLink, TcOptions};
 use aya::programs::{tc, Link, SchedClassifier, TcAttachType};
 use aya::{include_bytes_aligned, Bpf};
 use aya_log::BpfLogger;
 use clap::Parser;
-use core::time;
 use log::{info, warn};
 use serde::{Deserialize, Serialize};
-use std::{mem, thread};
+use serde_json;
+use std::mem;
+use std::{fs, io::BufReader};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
 struct Opt {
+    /// Interface to install filters on
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+    /// Whether to "install" or "delete"
+    #[clap(short, long, default_value = "load")]
+    action: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,6 +34,27 @@ struct TcProgram {
     direction: Direction,
     priority: u16,
     handle: u32,
+}
+
+const PATH: &str = "./saved_tc_link";
+
+impl TcProgram {
+    fn save(&self) -> Result<(), anyhow::Error> {
+        serde_json::to_writer(&fs::File::create(PATH).unwrap(), &self)?;
+        Ok(())
+    }
+
+    fn load() -> Result<Self, anyhow::Error> {
+        let file = fs::File::open(PATH)?;
+        let reader = BufReader::new(file);
+        let prog = serde_json::from_reader(reader)?;
+        Ok(prog)
+    }
+
+    fn delete() -> Result<(), anyhow::Error> {
+        fs::remove_file(PATH)?;
+        Ok(())
+    }
 }
 
 #[tokio::main]
@@ -56,129 +84,74 @@ async fn main() -> Result<(), anyhow::Error> {
     // the full cleanup can be done with 'sudo tc qdisc del dev eth0 clsact'.
     let _ = tc::qdisc_add_clsact(&opt.iface);
 
-    let delay = time::Duration::new(5, 0);
+    match opt.action.as_str() {
+        "load" => {
+            info!("loading program tctest1");
+            let program1: &mut SchedClassifier = bpf.program_mut("tctest1").unwrap().try_into()?;
+            program1.load()?;
 
-    let program0: &mut SchedClassifier = bpf.program_mut("tctest0").unwrap().try_into()?;
-    program0.load()?;
-    let link_id_0 = program0.attach(&opt.iface, TcAttachType::Ingress)?;
-    info!("\nlink_id_0: {:#?}", link_id_0);
+            info!("attaching program tctest1");
+            let link_id_1 = program1.attach_with_options(
+                &opt.iface,
+                TcAttachType::Ingress,
+                TcOptions {
+                    priority: 50,
+                    handle: 3,
+                },
+            )?;
 
-    info!("Sleep...");
-    thread::sleep(delay);
+            info!("calling take link for program 1 (tctest1)");
+            let link_1 = program1.take_link(link_id_1)?;
 
-    let program1: &mut SchedClassifier = bpf.program_mut("tctest1").unwrap().try_into()?;
-    program1.load()?;
-    let link_id_1 = program1.attach_with_options(
-        &opt.iface,
-        TcAttachType::Ingress,
-        TcOptions {
-            priority: 50,
-            handle: 3,
-        },
-    )?;
-    info!("\nlink_id_1: {:#?}", link_id_1);
+            let prog1_info = TcProgram {
+                if_name: opt.iface.clone(),
+                direction: Direction::Ingress,
+                priority: link_1.priority(),
+                handle: link_1.handle(),
+            };
 
-    info!("Sleep...");
-    thread::sleep(delay);
+            info!("saving program info");
+            prog1_info.save()?;
 
-    info!("calling take link for program 1 (tctest1)");
-    let link_1 = program1.take_link(link_id_1)?;
-    info!("\nlink_1: {:#?}", link_1);
+            mem::forget(link_1);
+        }
 
-    info!(
-        "priority: {}, handle: {}",
-        link_1.priority(),
-        link_1.handle()
-    );
+        "delete" => {
+            let prog1_info = TcProgram::load().unwrap_or_else(|error| {
+                panic!("Problem opening the file: {:?}", error);
+            });
 
-    let prog1_info = TcProgram {
-        if_name: opt.iface.clone(),
-        direction: Direction::Ingress,
-        priority: link_1.priority(),
-        handle: link_1.handle(),
-    };
+            let attach_type = match prog1_info.direction {
+                Direction::Ingress => TcAttachType::Ingress,
+                Direction::Egress => TcAttachType::Egress,
+            };
 
-    let attach_type = match prog1_info.direction {
-        Direction::Ingress => TcAttachType::Ingress,
-        Direction::Egress => TcAttachType::Egress,
-    };
+            let new_link_1 = SchedClassifierLink::new_tc_link(
+                &prog1_info.if_name,
+                attach_type,
+                prog1_info.priority,
+                prog1_info.handle,
+            )?;
 
-    let link_1_copy = SchedClassifierLink::new_tc_link(
-        &prog1_info.if_name,
-        attach_type,
-        prog1_info.priority,
-        prog1_info.handle,
-    )?;
+            info!("calling new_link_1.detach (tctest1)");
+            let detach_result = new_link_1.detach();
 
-    info!("\nlink_1_copy\n{:#?}", link_1_copy);
+            match detach_result {
+                Ok(()) => info!("new_link_1.detach() succeeded"),
+                Err(e) => info!("new_link_1.detach() failed. Error: {:#?}", e),
+            }
 
-    info!("Sleep...");
-    thread::sleep(delay);
+            info!("calling TcProgram::delete()");
+            let _ = TcProgram::delete();
+        }
 
-    info!("Adding program 2 (tctest2)");
-    let program2: &mut SchedClassifier = bpf.program_mut("tctest2").unwrap().try_into()?;
-    program2.load()?;
-    let link_id_2 = program2.attach_with_options(
-        &opt.iface,
-        TcAttachType::Ingress,
-        TcOptions {
-            priority: 50,
-            ..Default::default()
-        },
-    )?;
-    info!("\nlink_id_2: {:#?}", link_id_2);
-
-    info!("Sleep...");
-    thread::sleep(delay);
-
-    info!("calling take link for program 2 (tctest2)");
-    let link_2 = program2.take_link(link_id_2)?;
-    info!("\nlink_2: {:#?}\n", link_2);
-
-    info!(
-        "priority: {}, handle: {}",
-        link_2.priority(),
-        link_2.handle()
-    );
-
-    let _prog2_info = TcProgram {
-        if_name: opt.iface.clone(),
-        direction: Direction::Ingress,
-        priority: link_2.priority(),
-        handle: link_2.handle(),
-    };
-
-    mem::forget(link_2);
-
-    info!("Sleep...");
-    thread::sleep(delay);
-
-    info!("calling link_1_copy.detach (tctest1)");
-    link_1_copy.detach()?;
-
-    info!("Sleep...");
-    thread::sleep(delay);
-
-    info!("calling link_1.detach (tctest1)");
-    let result = link_1.detach();
-
-    match result {
-        Err(e) => info!("Error, as expected, calling link_1.detach: ({:#?})", e),
-        Ok(_) => info!("No error when calling link1.detach"),
-    };
-
-    let invalid_link = SchedClassifierLink::new_tc_link(&prog1_info.if_name, attach_type, 500, 4)?;
-
-    info!("calling invalid_link.detach (tctest1)");
-    let result = invalid_link.detach();
-
-    match result {
-        Err(e) => info!(
-            "Error, as expected, calling invalid_link.detach: ({:#?})",
-            e
-        ),
-        Ok(_) => info!("No error when calling link1.detach"),
-    };
+        _ => {
+            return Err(anyhow!(
+                "Invalid action: {}.  Valid actions are \"load\" and \"delete\"",
+                opt.action
+            ));
+        }
+    }
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
